6a2b456c328c345c574a13eb22fa12c09850f1a4
Gary Kerr <gdrummondk@gmail.com>
2019-12-20 22:00:21 +0000

    Repeating an Array

---
+++ AdventOfCode2019/AdventOfCode2019/Extensions/Array+Extensions.swift
@@ -0,0 +1,19 @@
+//
+//  Array+Extensions.swift
+//  AdventOfCode2019
+//
+//  Created by gary on 20/12/2019.
+//  Copyright © 2019 Gary Kerr. All rights reserved.
+//
+
+import Foundation
+
+extension Array {
+    func repeated(count: Int) -> Array<Element> {
+        var result = self
+        for _ in 0..<count - 1 {
+            result += self
+        }
+        return result
+    }
+}
