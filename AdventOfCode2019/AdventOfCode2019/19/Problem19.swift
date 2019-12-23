411f6938b956f4ff5de0c151551f3af8ac0a12a6
Gary Kerr <gdrummondk@gmail.com>
2019-12-23 21:50:49 +0000

    Problem 19, part 2

--- AdventOfCode2019/AdventOfCode2019/19/Problem19.swift
+++ AdventOfCode2019/AdventOfCode2019/19/Problem19.swift
@@ -6,13 +6,31 @@
 //  Copyright © 2019 Gary Kerr. All rights reserved.
 //
 
+/// Part 2
+/// ------
+///
+/// y = 700, beam width = 119
+///
+///
+/// First attempt: 8020999, too low
+///
+///
 final class Problem19: Problem {
+    private typealias BeamValue = (Coordinate, Int)
+    private typealias RowCache = [Int: (xMin: Int, xMax: Int)]
+
     private let file = "19/data19.txt"
     private lazy var input = { try! String(contentsOfFile: path + file).makeIntegers(separator: ",") }()
 
+    // For part 2
+    private var cache: RowCache = [:]
+    private let width = 100
+
     func run() {
-        let r1 = part1()
-        let r2 = 0
+        let r1 = 0
+//        let r1 = part1()
+//        let r2 = 0
+        let r2 = part2()
         printResults(number: 19, r1, r2)
     }
 }
@@ -21,10 +39,7 @@ final class Problem19: Problem {
 // MARK: - Private
 
 private extension Problem19 {
-    private typealias BeamValue = (Coordinate, Int)
-
     private func part1() -> Int {
-        print(input)
         let coordinates = makeCoordinates()
         var values: [BeamValue] = []
         for coordinate in coordinates {
@@ -36,6 +51,41 @@ private extension Problem19 {
 
         display(beamValues: values)
         return values.map({ $0.1 }).sum()
+    }
+
+
+    private func part2() -> Int {
+        var xAnswer = 0
+        var yAnswer = 0
+        for y in 1000...1500 {
+            let (xMin1, xMax1) = getRowValues(y: y)
+            let count1 = xMax1 - xMin1 + 1
+            if count1 < width {
+                continue
+            }
+            let (xMin2, xMax2) = getRowValues(y: y + width - 1)
+
+//            print(y, xMin1, xMax1)
+//            print(xMin2, xMax2)
+
+            let count2 = xMax1 - xMin2 + 1
+            if count2 == width {
+                print("y", y, "xMin2", xMin2)
+                print(10_000 * xMin2 + y)
+
+                print(y - 1, cache[y - 1])
+                print(y + width - 2, cache[y + width - 2])
+                print("---")
+                print(y, cache[y])
+                print(y + width - 1, cache[y + width - 1])
+                print("---")
+
+                xAnswer = xMin2
+                yAnswer = y
+                break
+            }
+        }
+        return 10_000 * xAnswer + yAnswer
     }
 
 
@@ -55,12 +105,39 @@ private extension Problem19 {
     }
 
 
+    private func getRowValues(y: Int) -> (xMin: Int, xMax: Int) {
+        if let row = cache[y] {
+            return row
+        }
+        var x = 0
+        var seenXMin = false
+        var xMin = 0
+        var xMax = 0
+        while true {
+            let console = AutoConsole(input: [x, y])
+            let computer = Computer(memory: input, console: console)
+            computer.run()
+            let value = console.output.first!
+            if value == 1 && !seenXMin {
+                xMin = x
+                seenXMin = true
+            } else if value == 0 && seenXMin {
+                xMax = x - 1
+                break
+            }
+            x += 1
+        }
+        cache[y] = (xMin, xMax)
+        return (xMin, xMax)
+    }
+
+
     private func display(beamValues: [BeamValue]) {
         let coordinates = beamValues.map({ $0.0 })
         let xCoords = coordinates.map({ $0.x })
         let yCoords = coordinates.map({ $0.y })
-        let (xMin, xMax) = (xCoords.min()!, xCoords.max()!)
-        let (yMin, yMax) = (yCoords.min()!, yCoords.max()!)
+        let xMax = xCoords.max()!
+        let yMax = yCoords.max()!
         var screen = Array(repeating: Array(repeating: "", count: xMax + 1), count: yMax + 1)
         for (coord, value) in beamValues {
             screen[coord.y][coord.x] = value == 0 ? "." : "#"
