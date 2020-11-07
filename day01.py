af0d8fd04a3c3c3c2363c44b7e422987085a3231
Gary Kerr <garykerrwork@gmail.com>
2020-11-07 19:51:06 +0000

    Day 1, part 2

--- day01.py
+++ day01.py
@@ -3,6 +3,7 @@
 TEST01 = 'R2, L3'
 TEST02 = 'R2, R2, R2'
 TEST03 = 'R5, L5, R5, R3'
+TEST04 = 'R8, R4, R4, R8'
 
 SOURCE = 'day01.txt'
 
@@ -41,7 +42,7 @@ def update_location(location, direction, turn, distance):
     return (x, y), direction
 
 
-def run(instructions):
+def run1(instructions):
     location = 0, 0
     direction = NORTH
     for turn, distance in instructions:
@@ -50,32 +51,53 @@ def run(instructions):
     return abs(x) + abs(y)
 
 
+def run2(instructions):
+    location = 0, 0
+    locations = set([location])
+    direction = NORTH
+    for turn, distance in instructions:
+        direction = update_direction(direction, turn)
+        for _ in range(1, distance + 1):
+            location = location[0] + direction[0], location[1] + direction[1]
+            if location in locations:
+                x, y = location
+                return abs(x) + abs(y)
+            else:
+                locations.add(location)
+    return 0
+
+
+
 def main1():
     instructions = make_instructions(open(SOURCE, 'r').read())
-    distance = run(instructions)
+    distance = run1(instructions)
     print(distance)
 
 
 def main2():
-    pass
+    instructions = make_instructions(open(SOURCE, 'r').read())
+    distance = run2(instructions)
+    print(distance)
 
 
 def test1():
     i1 = make_instructions(TEST01)
-    d1 = run(i1)
+    d1 = run1(i1)
     assert d1 == 5
 
     i2 = make_instructions(TEST02)
-    d2 = run(i2)
+    d2 = run1(i2)
     assert d2 == 2
 
     i3 = make_instructions(TEST03)
-    d3 = run(i3)
+    d3 = run1(i3)
     assert d3 == 12
 
 
 def test2():
-    pass
+    i1 = make_instructions(TEST04)
+    d1 = run2(i1)
+    assert d1 == 4
 
 
 if __name__ == '__main__':
