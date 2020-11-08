baaaff48ae54be8f4f9cb3583474fa16f4e93e6a
Gary Kerr <garykerrwork@gmail.com>
2020-11-08 17:32:02 +0000

    Day 7, part 2

--- day07.py
+++ day07.py
@@ -8,6 +8,11 @@ abcd[bddb]xyyx
 aaaa[qwer]tyui
 ioxxoj[asdfgh]zxcvbn'''
 
+TEST2 = '''aba[bab]xyz
+xyx[xyx]xyx
+aaa[kek]eke
+zazbz[bzb]cdb'''
+
 
 def split_ip(ip):
     outside = []
@@ -45,6 +50,23 @@ def supports_tls(outsides):
     return False
 
 
+def get_abas(parts):
+    abas = set()
+    for part in parts:
+        for i in range(len(part) - 2):
+            x = part[i:i + 3]
+            if x == x[::-1] and x[0] != x[1]:
+                abas.add(x)
+    return abas
+
+
+def supports_ssl(outsides, insides):
+    abas = get_abas(outsides)
+    babs = get_abas(insides)
+    babs = set([x[1:] + x[1] for x in babs])
+    return len(abas.intersection(babs)) != 0
+
+
 def main1():
     count = 0
     ips = open(SOURCE, 'r').read().strip().split()
@@ -56,6 +78,16 @@ def main1():
     print(count)
 
 
+def main2():
+    count = 0
+    ips = open(SOURCE, 'r').read().strip().split()
+    for ip in ips:
+        outsides, insides = split_ip(ip)
+        if supports_ssl(outsides, insides):
+            count += 1
+    print(count)
+
+
 def test1():
     for line in TEST1.split():
         print(line)
@@ -63,6 +95,15 @@ def test1():
         print(supports_tls(outsides))
 
 
+def test2():
+    for line in TEST2.split():
+        print(line)
+        outsides, insides = split_ip(line)
+        print(supports_ssl(outsides, insides))
+
+
 if __name__ == '__main__':
     # test1()
+    # test2()
     main1()
+    main2()
