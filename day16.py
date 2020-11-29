e51984fac1ea2d0d05072b1415f8ed8c8fb3c468
Gary Kerr <garykerrwork@gmail.com>
2020-11-29 00:05:43 +0000

    Day 16, part 1

---
+++ day16.py
@@ -0,0 +1,45 @@
+#!python3
+
+INPUT = '10001110011110000'
+LENGTH = 272
+
+TEST_INPUT = '10000'
+TEST_LENGTH = 20
+
+
+def lengthen(data, n):
+    while len(data) < n:
+        data = modified_dg(data)
+    return data[:n]
+
+
+def modified_dg(data):
+    other = ''.join('0' if x == '1' else '1' for x in data)[::-1]
+    return f'{data}0{other}'
+
+
+def checksum_step(data):
+    result = []
+    for i in range(0, len(data), 2):
+        x = data[i:i + 2]
+        if x == x[::-1]:
+            result.append('1')
+        else:
+            result.append('0')
+    return ''.join(result)
+
+
+def checksum(data):
+    while len(data) % 2 == 0:
+        data = checksum_step(data)
+    return data
+
+
+def main():
+    data = lengthen(INPUT, LENGTH)
+    x = checksum(data)
+    print(x)
+
+
+if __name__ == '__main__':
+    main()
