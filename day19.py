179bfa71d29a44500821a7ef74af86004136acd5
Gary Kerr <garykerrwork@gmail.com>
2020-11-29 23:21:23 +0000

    Day 19, part 1

---
+++ day19.py
@@ -0,0 +1,54 @@
+#!python3
+
+
+INPUT = 3_014_387
+
+
+class Elf(object):
+    def __init__(self, position, presents=1):
+        self.position = position
+        self.presents = presents
+        self.next = None
+
+
+    def steal(self):
+        # print(self.position, 'stealing from', self.next.position)
+        after_next = self.next.next
+        self.presents += self.next.presents
+        if after_next.position == self.position:
+            self.next = None
+        else:
+            self.next = after_next
+
+
+    def __repr__(self):
+        n = '?' if self.next is None else self.next.position
+        return f'Elf({self.position}, {self.presents}, {n})'
+
+
+def make_ring(n):
+    elfs = [Elf(i) for i in range(1, n + 1)]
+    for e1, e2 in zip(elfs, elfs[1:] + [elfs[0]]):
+        e1.next = e2
+    return elfs
+
+
+def part1():
+    elfs = make_ring(INPUT)
+    elf = elfs[0]
+    while True:
+        elf.steal()
+        if elf.next is None:
+            break
+        elf = elf.next
+    return elf.position
+
+
+
+def main():
+    p = part1()
+    print(p)
+
+
+if __name__ == '__main__':
+    main()
