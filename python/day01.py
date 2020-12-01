1cfa13720799a32f7a76bc63fa8d1f38ab92d19c
Gary Kerr <garykerrwork@gmail.com>
2020-12-01 21:09:01 +0000

    Day 1, part 2

--- python/day01.py
+++ python/day01.py
@@ -1,21 +1,32 @@
 #!python3
 
-
+from functools import reduce
 from itertools import combinations
 
 
 INPUT = 'python/day01.txt'
 
 
-def part1():
-    numbers = (int(line.strip()) for line in open(INPUT, 'r').readlines())
-    for n1, n2 in combinations(numbers, 2):
-        if n1 + n2 == 2020:
-            return n1 * n2
+def check(numbers, n):
+    for m in combinations(numbers, n):
+        if sum(m) == 2020:
+            return reduce(lambda x, y: x * y, m)
+
+
+def part1(numbers):
+    return check(numbers, 2)
+
+
+def part2(numbers):
+    return check(numbers, 3)
 
 
 def main():
-    p = part1()
+    numbers = [int(line.strip()) for line in open(INPUT, 'r').readlines()]
+    p = part1(numbers)
+    print(p)
+
+    p = part2(numbers)
     print(p)
 
 
