b6523dff2ccf2a0505e8d39884d8def9719d7ea8
Gary Kerr <garykerrwork@gmail.com>
2020-12-03 19:36:10 +0000

    Day 3, part 2

--- python/day03.py
+++ python/day03.py
@@ -1,5 +1,6 @@
 #!python.
 
+from math import prod
 
 INPUT = 'day03.txt'
 TEST_INPUT = '''..##.......
@@ -14,6 +15,13 @@ TEST_INPUT = '''..##.......
 #...##....#
 .#..#...#.#'''
 
+SLOPES = [
+    (1, 1), (1, 3), (1, 5), (1, 7), (2, 1)
+]
+TEST_SLOPE_ANSWERS = [
+    2, 7, 3, 4, 2
+]
+
 
 def make_map(input):
     lines = []
@@ -22,22 +30,47 @@ def make_map(input):
     return lines
 
 
-def count_trees(map):
+def count_trees(map, dr, dc):
     width = len(map[0])
     count = 0
+    r = 0
+    c = 0
     for (row, line) in enumerate(map):
-        col = (3 * row) % width
-        if line[col] == '#':
+        if row != r:
+            continue
+        if line[c] == '#':
             count += 1
+        r = r + dr
+        c = (c + dc) % width
     return count
+
+
+def test2(map, slopes, results):
+    for (dr, dc), result in zip(slopes, results):
+        count = count_trees(map, dr, dc)
+        assert count == result, f'{dr}, {dc}, {count}, {result}'
+
+
+def part2(map, slopes):
+    counts = []
+    for dr, dc in slopes:
+        count = count_trees(map, dr, dc)
+        counts.append(count)
+    return prod(counts)
 
 
 def main():
     test_map = make_map(TEST_INPUT.strip().split('\n'))
-    assert count_trees(test_map) == 7
+    assert count_trees(test_map, 1, 3) == 7
 
     map = make_map(open(INPUT, 'r').readlines())
-    p = count_trees(map)
+    p = count_trees(map, 1, 3)
+    print(p)
+
+    test2(test_map, SLOPES, TEST_SLOPE_ANSWERS)
+    assert part2(test_map, SLOPES) == 336
+
+    p = part2(map, SLOPES)
     print(p)
 
 
