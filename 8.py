6907d265db83b0fb43fd4d0cf766d26e8d9a89b8
Gary Kerr <garykerrwork@gmail.com>
2020-12-27 17:34:57 +0000

    Day 8, part 2

--- 8.py
+++ 8.py
@@ -1,7 +1,7 @@
 #!python
 '''
 2 3
-    0 3 
+    0 3
         m: 10 11 12
     1 1
         0 1
@@ -14,109 +14,96 @@ INPUT = '8.txt'
 
 TEST_INPUT = '''2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2'''
 
-READ_HEADER = 0
-READ_META_ITEMS = 1
-PARSE_HEADER = 2
-
-STATE_DESCRIPTION = {
-    READ_HEADER: 'read header',
-    READ_META_ITEMS: 'read meta items',
-    PARSE_HEADER: 'parse header'
-}
-
-
-class Data(object):
-    def __init__(self, data):
-        self.data = map(int, data.split(' '))
-    
-    def header(self):
-        return Header(next(self.data), next(self.data))
-        
-    def meta(self, n):
-        return [next(self.data) for _ in range(n)]
-        
-        
-class Header(object):
-    def __init__(self, child_count, meta_count):
-        self.child_count = child_count
-        self.meta_count = meta_count
-        
-    def has_no_children(self):
-        return self.child_count == 0
-        
-    def has_children(self):
-        return self.child_count > 0
-        
-    def remove_child(self):
-        self.child_count -= 1
-        
-    def __repr__(self):
-        return f'Header({self.child_count}, {self.meta_count})'
-
 
 def parse_input(data):
-    return list(map(int, data.split(' ')))
-    
-    
-def machine(data):
-    '''States
-        0: read header
-        1: read meta items
-        2: parse header
-    '''
-    headers = []
-    state = READ_HEADER
-    meta_items = []
-    
-    while True:
-        if state == READ_HEADER:
-            header = data.header()
-            headers.append(header)
-            state = PARSE_HEADER
-        
-        elif state == PARSE_HEADER:
-            if not headers: 
-                break
-            header = headers.pop()
-            if header.has_no_children():
-                state = READ_META_ITEMS
-            else:
-                state = READ_HEADER
-            headers.append(header)
-            
-        elif state == READ_META_ITEMS:
-            header = headers.pop()
-            meta_items.extend(data.meta(header.meta_count))
-            if header.has_children():
-                header.remove_child()
-                headers.append(header)
-            else:
-                if headers:
-                    header = headers.pop()
-                    header.remove_child()
-                    headers.append(header)
-                
-            state = PARSE_HEADER
+    return map(int, data.split(' '))
 
-    return meta_items
-        
-    
+
+class Node(object):
+    def __init__(self, child_count, meta_count, children, meta_data):
+        self.child_count = child_count
+        self.meta_count = meta_count
+        self.children = children
+        self.meta_data = meta_data
+        self.child_map = {i + 1: c for i, c in enumerate(children)}
+
+
+    def meta_data_sum(self):
+        s = 0
+        for child in self.children:
+            s += child.meta_data_sum()
+        return s + sum(self.meta_data)
+
+
+    def value(self):
+        assert self.child_count == len(self.children)
+        assert self.meta_count == len(self.meta_data)
+        if self.child_count == 0:
+            return sum(self.meta_data)
+        else:
+            s = 0
+            for n in self.meta_data:
+                child = self.child_map.get(n, None)
+                if child is not None:
+                    s += child.value()
+            return s
+
+
+    def __repr__(self):
+        c_count = len(self.children)
+        return f'Node({self.child_count}, {self.meta_count}, children: {c_count}, meta: {self.meta_data})'
+
+
+def make_nodes(data):
+    child_count = next(data)
+    meta_count = next(data)
+    children = []
+    meta_data = []
+
+    for _ in range(child_count):
+        child = make_nodes(data)
+        children.append(child)
+
+    for _ in range(meta_count):
+        meta_data.append(next(data))
+
+    return Node(child_count, meta_count, children, meta_data)
+
+
+def test1(data):
+    node = make_nodes(data)
+    assert node.meta_data_sum() == 138
+
+
 def part1(data):
-    data = Data(data)
-    meta_items = machine(data)
-    print(sum(meta_items))
-    
-    
+    node = make_nodes(data)
+    return node.meta_data_sum()
+
+
+def test2(data):
+    node = make_nodes(data)
+    assert node.value() == 66
+
+
+def part2(data):
+    node = make_nodes(data)
+    return node.value()
+
+
+
 def main():
-    with open(INPUT, 'r') as f:
-        part1(f.read())
-    
-    
-def test():
-    part1(TEST_INPUT)
-    
-    
+    test1(parse_input(TEST_INPUT))
+
+    data1 = parse_input(open(INPUT, 'r').read())
+    p = part1(data1)
+    print(p)
+
+    test2(parse_input(TEST_INPUT))
+
+    data2 = parse_input(open(INPUT, 'r').read())
+    p =part2(data2)
+    print(p)
+
+
 if __name__ == '__main__':
     main()
-    
-    test()
