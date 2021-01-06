e29c3490616309fd151cd0fdf393176485f5cd37
Gary Kerr <garykerrwork@gmail.com>
2021-01-06 22:26:22 +0000

    Day 3, part 2

--- c++/day03.cpp
+++ c++/day03.cpp
@@ -9,8 +9,9 @@ using namespace std;
 
 void test1();
 int part1(vector<string>);
+int64_t part2(vector<string>);
 vector<string> make_map();
-int tree_count(vector<string>, int);
+int tree_count(vector<string>, int, int);
 
 string TEST_INPUT = R"(..##.......
 #...#...#..
@@ -30,21 +31,23 @@ int main() {
 
     test1();
     int p1 = part1(map);
-
     cout << p1 << endl;
+
+    int64_t p2 = part2(map);
+    cout << p2 << endl;
     return 0;
 }
 
 
-int tree_count(vector<string> map, int offset) {
+int tree_count(vector<string> map, int right, int down) {
     int width = map.at(0).size();
-    int count{ 0 };
-    int column{ 0 };
-    for (int i = 0; i < map.size(); i++) {
+    int count { 0 };
+    int column { 0 };
+    for (int i = 0; i < map.size(); i = i + down) {
         if (map.at(i).at(column) == '#') {
             count += 1;
         }
-        column = (column + offset) % width;
+        column = (column + right) % width;
     }
     return count;
 }
@@ -62,7 +65,22 @@ vector<string> make_map() {
 
 
 int part1(vector<string> map) {
-    return tree_count(map, 3);
+    return tree_count(map, 3, 1);
+}
+
+
+int64_t part2(vector<string> map) {
+    vector<pair<int, int>> pairs{
+        make_pair(1, 1), make_pair(3, 1), make_pair(5, 1), make_pair(7, 1),
+        make_pair(1, 2)
+    };
+    int c;
+    int64_t t { 1 };
+    for (pair<int, int> p: pairs) {
+        c = tree_count(map, p.first, p.second);
+        t = t * static_cast<int64_t>(c);
+    }
+    return t;
 }
 
 
@@ -73,6 +91,6 @@ void test1() {
     while (getline(os, line)) {
         map.push_back(line);
     }
-    int c = tree_count(map, 3);
+    int c = tree_count(map, 3, 1);
     assert(c == 7);
 }
