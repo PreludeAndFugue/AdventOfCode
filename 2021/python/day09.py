db2a7aa78ecd2038a37f1f7b870851707fd57905
Gary Kerr <garykerrwork@gmail.com>
2021-12-09 20:47:25 +0000

    2021, day 9, part 2, python

--- 2021/python/day09.py
+++ 2021/python/day09.py
@@ -20,20 +20,58 @@ def parse(string):
 
 def neighbour_heights(map, row, col):
     return [
-        map.get((row - 1, col), 1_000_000),
-        map.get((row + 1, col), 1_000_000),
-        map.get((row, col - 1), 1_000_000),
-        map.get((row, col + 1), 1_000_000),
+        map.get((row - 1, col), 10),
+        map.get((row + 1, col), 10),
+        map.get((row, col - 1), 10),
+        map.get((row, col + 1), 10),
     ]
 
 
-def part1(map):
+def pop_neighbours(map_, location):
+    row, col = location
+    neighbours = []
+    for n in [(row - 1, col), (row + 1, col), (row, col - 1), (row, col + 1)]:
+        h = map_.pop(n, 10)
+        if h < 9:
+            neighbours.append(n)
+    return neighbours
+
+
+def get_basin(map_, location):
+    basin = []
+    seen = set()
+    to_search = [location]
+    while to_search:
+        l = to_search.pop(0)
+        if l in seen:
+            continue
+        basin.append(l)
+        seen.add(l)
+        neighbours = pop_neighbours(map_, l)
+        to_search.extend(neighbours)
+    return basin
+
+
+def part1(map_):
     minima = []
-    for location, height in map.items():
-        nh = neighbour_heights(map, *location)
+    for location, height in map_.items():
+        nh = neighbour_heights(map_, *location)
         if height < min(nh):
             minima.append(height)
     return sum(n + 1 for n in minima)
+
+
+def part2(map_):
+    map_ = {k: v for k, v in map_.items() if v < 9}
+    basins = []
+    while map_:
+        location, _ = map_.popitem()
+        basin = get_basin(map_, location)
+        basins.append(basin)
+    answer = 1
+    for n in sorted(len(b) for b in basins)[-3:]:
+        answer *= n
+    return answer
 
 
 def main():
@@ -45,6 +83,12 @@ def main():
 
     p1 = part1(map_)
     print(f'Part 1: {p1}')
+
+    t2 = part2(test_map)
+    assert t2 == 1134
+
+    p2 = part2(map_)
+    print(f'Part 2: {p2}')
 
 
 if __name__ == '__main__':
