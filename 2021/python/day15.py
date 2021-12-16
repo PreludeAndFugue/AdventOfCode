01da530f6d539638ae112ef8848cf1b88dc8ce4f
Gary Kerr <garykerrwork@gmail.com>
2021-12-16 01:18:38 +0000

    2021, day 15, part 1, python

--- 2021/python/day15.py
+++ 2021/python/day15.py
@@ -5,6 +5,7 @@
 '''
 
 import csv
+import heapq
 from functools import cache
 
 from helpers import BASE
@@ -29,80 +30,35 @@ def parse(string):
     return map_
 
 
-def min_rd_neighbours(location, map_):
+def neighbours(location, input_map):
     x, y = location
-    return min(map_.get((x + 1, y), 10_000_000), map_.get((x, y + 1), 10_000_000))
-
-
-def make_csv(input_map, file_name):
-    m = max(input_map)
-    x_max = m[0] + 1
-    y_max = m[1] + 1
-    with open(file_name, 'w') as f:
-        writer = csv.writer(f)
-        for y in range(x_max):
-            row = []
-            for x in range(y_max):
-                row.append(input_map[x, y])
-            writer.writerow(row)
+    ns = [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
+    for n in ns:
+        if n in input_map:
+            yield n, input_map[n]
 
 
 def part1(input_map):
-    '''Dynamic programming building up minimum sum from bottom right.
-    Minimum sums are generated in the input dict.'''
-    # make_csv(input_map, 'day15_1.csv')
-    initial = input_map[0, 0]
-    n = sum(max(input_map))
-    for i in range(n - 1, -1, -1):
-        for location in (l for l in input_map if sum(l) == i):
-            input_map[location] += min_rd_neighbours(location, input_map)
-    # make_csv(input_map, 'day15_2.csv')
-    return input_map[0, 0] - initial
+    '''Dijkstra's algorithm.'''
+    start = min(input_map)
+    end = max(input_map)
+    distance = {l: 1_000_000_000 for l in input_map}
+    distance[start] = 0
+    processed = {l: False for l in input_map}
+    q = [(0, start)]
+    counter = 0
+    while q:
+        counter += 1
+        q.sort()
+        _, a = q.pop(0)
+        if not processed[a]:
+            processed[a] = True
+            for b, w in neighbours(a, input_map):
+                if distance[a] + w < distance[b]:
+                    distance[b] = distance[a] + w
+                    q.append((distance[b], b))
+    return distance[end]
 
-
-def part1a(input_map):
-    '''Recursive function starting in top left.'''
-    @cache
-    def do_sum(location):
-        x, y = location
-        if x == 0 and y == 0:
-            return 0
-        if x == 0:
-            return do_sum((x, y - 1)) + input_map[location]
-        if y == 0:
-            return do_sum((x - 1, y)) + input_map[location]
-        return min(do_sum((x, y - 1)), do_sum((x - 1, y))) + input_map[location]
-
-    m = max(input_map)
-    x_max = m[0] + 1
-    y_max = m[1] + 1
-    sums = {(0, 0): 0}
-    for y in range(y_max):
-        for x in range(x_max):
-            location = (x, y)
-            sums[location] = do_sum(location)
-    return sums[m]
-
-
-def part1b(input_map):
-    '''Dynamic programming building up minimum sum from bottom right.
-    Similar to part1, but sums are stored in a separate dict.'''
-    m = max(input_map)
-    x_max = m[0]
-    y_max = m[1]
-    sums = {}
-    sums[m] = input_map[m]
-    for x in range(x_max - 1, -1, -1):
-        sums[x, y_max] = input_map[x, y_max] + sums[x + 1, y_max]
-    for y in range(y_max - 1, -1, -1):
-        sums[x_max, y] = input_map[x_max, y] + sums[x_max, y + 1]
-    for y in range(y_max - 1, -1, -1):
-        for x in range(x_max - 1, -1, -1):
-            l = x, y
-            l1 = x + 1, y
-            l2 = x, y + 1
-            sums[l] = input_map[l] + min(sums.get(l1, 1_000_000), sums.get(l2, 1_000_000))
-    return sums[0, 0] - input_map[0, 0]
 
 
 def main():
@@ -112,19 +68,8 @@ def main():
     t1 = part1(test_map.copy())
     assert t1 == 40
 
-    print(t1)
-    t1a = part1a(test_map.copy())
-    print(t1a)
-    t1b = part1b(test_map.copy())
-    print(t1b)
-
     p1 = part1(map_.copy())
     print(f'Part 1: {p1}')
-
-    p1a = part1a(map_.copy())
-    print(p1a)
-    p1b = part1b(map_.copy())
-    print(p1b)
 
 
 if __name__ == '__main__':
