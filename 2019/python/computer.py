fe90eb65f30d8a087e4122ad7812b0c1687c7072
Gary Kerr <garykerrwork@gmail.com>
2021-12-21 22:51:23 +0000

    2019, day 5, part 2, python

--- 2019/python/computer.py
+++ 2019/python/computer.py
@@ -14,6 +14,7 @@ class IntcodeComputer(object):
     def run(self):
         while True:
             opcode = self.memory[self.pointer]
+            # print('opcode', opcode)
             opcode_n = opcode % 100
             mode1 = (opcode // 100) % 10
             mode2 = (opcode // 1000) % 10
@@ -26,6 +27,14 @@ class IntcodeComputer(object):
                 self._input(mode1, mode2, mode3)
             elif opcode_n == 4:
                 self._output(mode1, mode2, mode3)
+            elif opcode_n == 5:
+                self._jump_if_true(mode1, mode2, mode3)
+            elif opcode_n == 6:
+                self._jump_if_false(mode1, mode2, mode3)
+            elif opcode_n == 7:
+                self._less_than(mode1, mode2, mode3)
+            elif opcode_n == 8:
+                self._equals(mode1, mode2, mode3)
             elif opcode_n == 99:
                 break
             else:
@@ -33,6 +42,7 @@ class IntcodeComputer(object):
 
 
     def _add(self, mode1, mode2, mode3):
+        '''Opcode 1'''
         assert mode3 == 0
         p1 = self.memory[self.pointer + 1]
         p2 = self.memory[self.pointer + 2]
@@ -45,6 +55,7 @@ class IntcodeComputer(object):
 
 
     def _multiply(self, mode1, mode2, mode3):
+        '''Opcode 2'''
         assert mode3 == 0
         p1 = self.memory[self.pointer + 1]
         p2 = self.memory[self.pointer + 2]
@@ -57,6 +68,7 @@ class IntcodeComputer(object):
 
 
     def _input(self, mode1, mode2, mode3):
+        '''Opcode 3'''
         assert mode1 == 0 and mode2 == 0 and mode3 == 0
         n = int(sys.stdin.readline().strip())
         p1 = self.memory[self.pointer + 1]
@@ -65,8 +77,59 @@ class IntcodeComputer(object):
 
 
     def _output(self, mode1, mode2, mode3):
+        '''Opcode 4'''
         assert mode2 == 0 and mode3 == 0
         p1 = self.memory[self.pointer + 1]
         n = self.memory[p1] if mode1 == 0 else p1
         sys.stdout.write(f'{n}\n')
         self.pointer += 2
+
+
+    def _jump_if_true(self, mode1, mode2, mode3):
+        '''Opcode 5'''
+        assert mode3 == 0
+        p1 = self.memory[self.pointer + 1]
+        n = self.memory[p1] if mode1 == 0 else p1
+        if n != 0:
+            p2 = self.memory[self.pointer + 2]
+            new_p = self.memory[p2] if mode2 == 0 else p2
+            self.pointer = new_p
+        else:
+            self.pointer += 3
+
+
+    def _jump_if_false(self, mode1, mode2, mode3):
+        '''Opcode 6'''
+        assert mode3 == 0
+        p1 = self.memory[self.pointer + 1]
+        n = self.memory[p1] if mode1 == 0 else p1
+        if n == 0:
+            p2 = self.memory[self.pointer + 2]
+            new_p = self.memory[p2] if mode2 == 0 else p2
+            self.pointer = new_p
+        else:
+            self.pointer += 3
+
+
+    def _less_than(self, mode1, mode2, mode3):
+        '''Opcode 7'''
+        assert mode3 == 0
+        p1 = self.memory[self.pointer + 1]
+        p2 = self.memory[self.pointer + 2]
+        p3 = self.memory[self.pointer + 3]
+        n1 = self.memory[p1] if mode1 == 0 else p1
+        n2 = self.memory[p2] if mode2 == 0 else p2
+        self.memory[p3] = 1 if n1 < n2 else 0
+        self.pointer += 4
+
+
+    def _equals(self, mode1, mode2, mode3):
+        '''Opcode 8'''
+        assert mode3 == 0
+        p1 = self.memory[self.pointer + 1]
+        p2 = self.memory[self.pointer + 2]
+        p3 = self.memory[self.pointer + 3]
+        n1 = self.memory[p1] if mode1 == 0 else p1
+        n2 = self.memory[p2] if mode2 == 0 else p2
+        self.memory[p3] = 1 if n1 == n2 else 0
+        self.pointer += 4
--- 2019/python/day05.py
+++ 2019/python/day05.py
@@ -8,20 +8,62 @@ TEST01 = '''3,0,4,0,99'''
 
 TEST02 = '''1002,4,3,4,33'''
 
+TEST03 = '''3,9,8,9,10,9,4,9,99,-1,8'''
+
+TEST04 = '''3,9,7,9,10,9,4,9,99,-1,8'''
+
+TEST05 = '''3,3,1108,-1,8,3,4,3,99'''
+
+TEST06 = '''3,3,1107,-1,8,3,4,3,99'''
+
+TEST07 = '''3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9'''
+
+TEST08 = '''3,3,1105,-1,9,1101,0,0,12,4,12,99,1'''
+
+TEST09 = '''3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99'''
+
 
 def tests():
     # t1 = list(map(int, TEST01.split(',')))
     # c1 = IntcodeComputer(t1)
     # c1.run()
 
-    t2 = list(map(int, TEST02.split(',')))
-    c2 = IntcodeComputer(t2)
-    c2.run()
-    assert c2.memory[-1] == 99
+    # t2 = list(map(int, TEST02.split(',')))
+    # c2 = IntcodeComputer(t2)
+    # c2.run()
+    # assert c2.memory[-1] == 99
+
+    # t3 = list(map(int, TEST03.split(',')))
+    # c3 = IntcodeComputer(t3)
+    # c3.run()
+
+    # t4 = list(map(int, TEST04.split(',')))
+    # c4 = IntcodeComputer(t4)
+    # c4.run()
+
+    # t5 = list(map(int, TEST05.split(',')))
+    # c5 = IntcodeComputer(t5)
+    # c5.run()
+
+    # t6 = list(map(int, TEST06.split(',')))
+    # c6 = IntcodeComputer(t6)
+    # c6.run()
+
+    # t7 = list(map(int, TEST07.split(',')))
+    # c7 = IntcodeComputer(t7)
+    # c7.run()
+
+    # t8 = list(map(int, TEST08.split(',')))
+    # c8 = IntcodeComputer(t8)
+    # c8.run()
+
+    t9 = list(map(int, TEST09.split(',')))
+    c9 = IntcodeComputer(t9)
+    c9.run()
 
 
 def part1():
-    tests()
+    # tests()
 
     program = list(map(int, open(BASE + 'day05.txt').read().strip().split(',')))
     c = IntcodeComputer(program)
