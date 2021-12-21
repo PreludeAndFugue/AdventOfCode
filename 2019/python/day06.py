ebeee7096d8a91b89929f5297ffa4af25500cd90
Gary Kerr <garykerrwork@gmail.com>
2021-12-21 23:28:01 +0000

    2019, day 6, part 2, python

--- 2019/python/day06.py
+++ 2019/python/day06.py
@@ -16,6 +16,20 @@ E)J
 J)K
 K)L'''
 
+TEST02 = '''COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L
+K)YOU
+I)SAN'''
+
 
 def parse(string):
     orbits = defaultdict(list)
@@ -23,6 +37,22 @@ def parse(string):
         a, b = line.split(')')
         orbits[a].append(b)
     return orbits
+
+
+def reverse_orbits(orbits):
+    r = {}
+    for k, v in orbits.items():
+        for m in v:
+            r[m] = k
+    return r
+
+
+def path(p, orbits):
+    result = []
+    while p:
+        p = orbits.get(p, None)
+        result.append(p)
+    return result
 
 
 def part1(orbits):
@@ -38,15 +68,31 @@ def part1(orbits):
     return count
 
 
+def part2(orbits):
+    r = reverse_orbits(orbits)
+    s_path = path('SAN', r)
+    y_path = path('YOU', r)
+    for i, m in enumerate(y_path):
+        if m in s_path:
+            return i + s_path.index(m)
+
+
 def main():
-    test_orbits = parse(TEST01)
-    t1 = part1(test_orbits)
+    test_orbits_1 = parse(TEST01)
+    t1 = part1(test_orbits_1)
     assert t1 == 42
 
     orbits = parse(open(BASE + 'day06.txt', 'r').read())
 
     p1 = part1(orbits)
     print(f'Part 1: {p1}')
+
+    test_orbits_2 = parse(TEST02)
+    t2 = part2(test_orbits_2)
+    assert t2 == 4
+
+    p2 = part2(orbits)
+    print(f'Part 2: {p2}')
 
 
 if __name__ == '__main__':
