0ac590e4181f9a8a61bd305c659647dd2c59db29
Gary Kerr <garykerrwork@gmail.com>
2021-12-25 13:56:02 +0000

    2021, day 24, python

--- 2021/python/day24.py
+++ 2021/python/day24.py
@@ -19,107 +19,123 @@ def make_input_numbers(n):
 
 
 def run(input_numbers):
+    '''
+    w_6 = w_5 - 2
+    w_4 = w_7 - 7
+    w_8 = w_9 - 4
+    w_11 = w_10 - 6
+    w_3 = w_12 - 5
+    w_13 = w_2 - 7
+    w_1 = w_14
+
+    # Largest
+
+    w_1 = 9
+    w_2 = 9
+    w_3 = 4
+    w_4 = 2
+    w_5 = 9
+    w_6 = 7
+    w_7 = 9
+    w_8 = 5
+    w_9 = 9
+    w_10 = 9
+    w_11 = 3
+    w_12 = 9
+    w_13 = 2
+    w_14 = 9
+
+    z = 99429795993929
+
+    # Smallest
+
+    w_1 = 1
+    w_2 = 8
+    w_3 = 1
+    w_4 = 1
+    w_5 = 3
+    w_6 = 1
+    w_7 = 8
+    w_8 = 1
+    w_9 = 5
+    w_10 = 7
+    w_11 = 1
+    w_12 = 6
+    w_13 = 1
+    w_14 = 1
+
+    z = 18113181571611
+
+    '''
     x, y, z = 0, 0, 0
     x = 1
 
-    w_1 = next(input_numbers)
-    w_2 = next(input_numbers)
-    w_3 = next(input_numbers)
-    w_4 = next(input_numbers)
-    w_5 = next(input_numbers)
-    w_6 = next(input_numbers)
-    w_7 = next(input_numbers)
-    w_8 = next(input_numbers)
-    w_9 = next(input_numbers)
-    w_10 = next(input_numbers)
-    w_11 = next(input_numbers)
-    w_12 = next(input_numbers)
-    w_13 = next(input_numbers)
-    w_14 = next(input_numbers)
+    w_6 = 7
+    w_8 = 5
+
+    # print(input_numbers)
+    w_1, w_2, w_3, w_4, w_5, w_7, w_9, w_10, w_11, w_12, w_13, w_14 = input_numbers
+    ws = (w_1, w_2, w_3, w_4, w_5, w_6, w_7, w_8, w_9, w_10, w_11, w_12, w_13, w_14)
+    # print(w_1, w_2, w_3, w_4, w_5, w_6, w_7, w_8, w_9, w_10, w_11, w_12, w_13, w_14)
 
     z = 26 * (26 * (26 * (w_1 + 2) + w_2 + 4) + w_3 + 8) + w_4 + 7
-    x = 1 if (w_5 - 2) != w_6 else 0
-    if x == 1:
-        # z = 26 * z + w_6 + 7
+    # x = 1 if (w_5 - 2) != w_6 else 0
 
-        x = w_6 + 7
+    if (w_5 - 2) != w_6:
+        return 10_000, 1
+
+    if (w_4 + 7) != w_7:
+        return 10_000, 1
+
+    if (w_8 + 4) != w_9:
+        return 10_000, 1
+
+    if (w_10 - 6) != w_11:
+        return 10_000, 1
+
+    if (w_3 + 5) != w_12:
+        return 10_000, 1
+
+    if (w_2 - 7) != w_13:
+        return 10_000, 1
+
+    if w_1 != w_14:
+        return 10_000, 1
+
+
+    # if (w_5 - 2) != w_6:
+
+        # raise ValueError
+
+        # if (w_6 + 7) != w_7:
+        #     z = 26 * z + w_7 + 10
+
+        # if (w_8 + 4) != w_9:
+        #     z = 26 * z + w_9 + 2
+
+        # if (w_10 - 6) != w_11:
+        #     z = 26 * z + w_11 + 8
+
+        # x = z % 26 - 3
         # z = z // 26
-        x = 1 if (w_6 + 7) != w_7 else 0
-        if x == 1:
-            z = 26 * z + w_7 + 10
+        # if x != w_12:
+        #     z = 26 * z + w_12 + 11
 
-        x = 1 if (w_8 + 4) != w_9 else 0
-        if x == 1:
-            z = 26 * z + w_9 + 2
+        # x = z % 26 - 11
+        # z = z // 26
+        # if x != w_13:
+        #     z = 26 * z + w_13 + 5
 
-        x = 1 if (w_10 - 6) != w_11 else 0
-        if x == 1:
-            z = 26 * z + w_11 + 8
-
-        x = z % 26 - 3
-        z = z // 26
-        x = 1 if x != w_12 else 0
-        if x == 1:
-            z = 26 * z + w_12 + 11
-
-        x = z % 26 - 11
-        z = z // 26
-        x = 1 if x != w_13 else 0
-        if x == 1:
-            z = 26 * z + w_13 + 5
-
-        x = z % 26 - 2
-        z = z // 26
-        x = 1 if x != w_14 else 0
-        if x == 1:
-            z = 26 * z + w_14 + 11
-    else:
-        x = w_4 + 7
-        z = 26 * (26 * (w_1 + 2) + w_2 + 4) + w_3 + 8
-        x = 1 if x != w_7 else 0
-        if x == 1:
-            z = 26 * z + w_7 + 10
-
-        x = 1 if (w_8 + 4) != w_9 else 0
-        if x == 1:
-            z = 26 * z + w_9 + 2
-
-        x = 1 if (w_10 - 6) != w_11 else 0
-        if x == 1:
-            z = 26 * z + w_11 + 8
-
-        x = z % 26 - 3
-        z = z // 26
-        x = 1 if x != w_12 else 0
-        if x == 1:
-            z = 26 * z + w_12 + 11
-
-        x = z % 26 - 11
-        z = z // 26
-        x = 1 if x != w_13 else 0
-        if x == 1:
-            z = 26 * z + w_13 + 5
-
-        x = z % 26 - 2
-        z = z // 26
-        x = 1 if x != w_14 else 0
-        if x == 1:
-            z = 26 * z + w_14 + 11
-
-
-
-
-
-
-
-    # x = z % 26
-    # z = z // 26
-    # x = 1 if x != w_7 else 0
-    # if x == 1:
+        # x = z % 26 - 2
+        # z = z // 26
+        # if x != w_14:
+        #     z = 26 * z + w_14 + 11
+    # else:
+    z = 26 * (26 * (w_1 + 2) + w_2 + 4) + w_3 + 8
+    # if (w_4 + 7) != w_7:
     #     z = 26 * z + w_7 + 10
 
-    # x = 1 if (w_8 + 4) != w_9 else 0
-    # if x == 1:
+    # if (w_8 + 4) != w_9:
     #     z = 26 * z + w_9 + 2
 
     # x = 1 if (w_10 - 6) != w_11 else 0
@@ -127,35 +143,41 @@ def run(input_numbers):
     #     z = 26 * z + w_11 + 8
 
     # x = z % 26 - 3
+    # x = w_3 + 5
     # z = z // 26
-    # x = 1 if x != w_12 else 0
-    # if x == 1:
+    z = 26 * (w_1 + 2) + w_2 + 4
+    # if (w_3 + 5) != w_12:
     #     z = 26 * z + w_12 + 11
 
     # x = z % 26 - 11
+    # x = (w_2 + 4) - 11
     # z = z // 26
-    # x = 1 if x != w_13 else 0
-    # if x == 1:
+    z = (w_1 + 2)
+    # if (w_2 - 7) != w_13:
     #     z = 26 * z + w_13 + 5
 
     # x = z % 26 - 2
-    # z = z // 26
-    # x = 1 if x != w_14 else 0
-    # if x == 1:
+    # x = (w_1 + 2) - 2
+    # x = w_1
+    z = z // 26
+    # if w_1 != w_14:
     #     z = 26 * z + w_14 + 11
 
-    return z
+    return z, ws
 
 
 def part1():
     current_min = 1_000_000_000_000_000_000
-    for numbers in product([9, 8, 7, 6, 5, 4, 3, 2, 1], repeat=14):
+    for numbers in product([9, 8, 7, 6, 5, 4, 3, 2, 1], repeat=12):
+        # n = sum(m * 10 ** i for i, m in enumerate(reversed(numbers)))
+        # if n % 26 : continue
         # print(numbers)
-        z = run(iter(numbers))
+        z, x = run(numbers)
         if z < current_min:
             current_min = z
-            print(numbers)
-            print(z)
+            print(x)
+            # print(numbers, n, n % 26)
+            print(z, z % 26)
         # print(z)
 
 
@@ -167,20 +189,35 @@ def main():
         99999999999999,
 
         12345678912345,
+
+        # w_5 = w_6 + 2
+        99999799999999,
+        # w_4 = w_7 - 7
+        99929799999999,
+        #
+        99929795999999,
+
+        99429795993929,
+        18113181571611,
     ]
     for n in numbers:
         input_numbers = make_input_numbers(n)
-        z = run(input_numbers)
-        print(z)
+        print(n)
+        # z = run(input_numbers)
+        # print(z)
 
         input_numbers = make_input_numbers(n)
         c = ALU(program, input_numbers)
         c.run()
         print(c.memory)
 
-        assert z == c.memory['z']
+        # assert z == c.memory['z']
 
-    part1()
+    # part1()
+
+    print('Part 1: 99429795993929')
+    print('Part 2: 18113181571611')
+
 
 
 if __name__ == '__main__':
