e6ebcb840ad1161ea71d078f2e94bd92e5b1f872
Gary Kerr <garykerrwork@gmail.com>
2021-12-26 19:48:07 +0000

    2019, day 9, part 2, python

--- 2019/python/day09.py
+++ 2019/python/day09.py
@@ -11,11 +11,21 @@ def part1(program):
     return c.readline()
 
 
+def part2(program):
+    c = Console([2])
+    ic = IntcodeComputer(program, c)
+    ic.run()
+    return c.readline()
+
+
 def main():
     program = list(map(int, open(BASE + 'day09.txt', 'r').read().strip().split(',')))
 
     p1 = part1(program)
     print(f'Part 1: {p1}')
+
+    p2 = part2(program)
+    print(f'Part 2: {p2}')
 
 
 if __name__ == '__main__':
