2795437555e74cf42c3e584240fc5a478071558c
Gary Kerr <gary.kerr@tutanota.com>
2022-12-01 18:35:23 +0000

    2022, day 1

--- 2022/swift/AdventOfCode2022/AdventOfCode2022/Days/Day01.swift
+++ 2022/swift/AdventOfCode2022/AdventOfCode2022/Days/Day01.swift
@@ -14,11 +14,22 @@ struct Day01: Day {
 
     func run() throws -> (String, String) {
         let s = try getInput()
-
-        // Parse input
-        
-
-        return (part1(input: s), part2(input: s))
+        var all: [[Int]] = []
+        var current: [Int] = []
+        for line in s.trimmingCharacters(in: .whitespaces).split(separator: "\n", omittingEmptySubsequences: false) {
+            if line.isEmpty {
+                all.append(current)
+                current = []
+            } else {
+                let n = Int(line)!
+                current.append(n)
+            }
+        }
+        if !current.isEmpty {
+            all.append(current)
+        }
+        let ns = all.map({ $0.reduce(0, +) }).sorted()
+        return (part1(input: ns), part2(input: ns))
     }
 }
 
@@ -26,12 +37,16 @@ struct Day01: Day {
 // MARK: - Private
 
 private extension Day01 {
-    func part1(input: String) -> String {
-        ""
+    func part1(input: [Int]) -> String {
+        let n = input.last!
+        return "\(n)"
     }
 
 
-    func part2(input: String) -> String {
-        ""
+    func part2(input: [Int]) -> String {
+        let start = input.index(input.endIndex, offsetBy: -3)
+        let end = input.endIndex
+        let m = input[start..<end].reduce(0, +)
+        return "\(m)"
     }
 }
