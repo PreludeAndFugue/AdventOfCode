d79d1ad15f3e81cac402437bff40da3ece25aa0e
Gary Kerr <gary.kerr@tutanota.com>
2022-12-01 18:37:31 +0000

    2022, day 2 preparation

---
+++ 2022/swift/AdventOfCode2022/AdventOfCode2022/Days/Day02.swift
@@ -0,0 +1,32 @@
+//
+//  Day02.swift
+//  AdventOfCode2022
+//
+//  Created by gary on 01/12/2022.
+//
+
+import Foundation
+
+struct Day02: Day {
+    let d = "02"
+
+
+    func run() throws -> (String, String) {
+
+        return ("", "")
+    }
+}
+
+
+// MARK: - Private
+
+private extension Day02 {
+    func part1() {
+
+    }
+
+
+    func part2() {
+        
+    }
+}
