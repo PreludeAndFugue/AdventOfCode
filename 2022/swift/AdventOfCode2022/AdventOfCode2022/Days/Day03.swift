369b5c1544f0074d412f425bb4f5e5e0e1f96e69
Gary Kerr <gary.kerr@tutanota.com>
2022-12-03 13:36:53 +0000

    2022, day 3

--- 2022/swift/AdventOfCode2022/AdventOfCode2022/Days/Day03.swift
+++ 2022/swift/AdventOfCode2022/AdventOfCode2022/Days/Day03.swift
@@ -8,12 +8,78 @@
 import Foundation
 
 struct Day03: Day {
+    static let lowercase = "abcdefghijklmnopqrstuvwxyz"
+    static let uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    private let test1 = """
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+"""
+
     let d = "03"
+    let priority: [Character: Int]
+
+
+    init() {
+        var p: [Character: Int] = [:]
+        for (i, c) in Self.lowercase.enumerated() {
+            p[c] = i + 1
+            p[c.uppercased().first!] = i + 27
+        }
+        priority = p
+    }
 
 
     func run() throws -> (String, String) {
         let s = try getInput()
+        let parts = s.trimmingCharacters(in: .whitespacesAndNewlines)
+            .split(separator: "\n")
+            .map({ String($0) })
+        return (part1(parts: parts), part2(parts: parts))
+    }
+}
 
-        return ("", "")
+
+private extension Day03 {
+    func part1(parts: [String]) -> String {
+        var prioritySum = 0
+        for part in parts {
+            let l = part.count
+            assert(l % 2 == 0)
+            let m = l / 2
+            let middle = part.index(part.startIndex, offsetBy: m)
+            let p1 = Set(part[..<middle])
+            let p2 = Set(part[middle...])
+            let i = p1.intersection(p2)
+            assert(i.count == 1)
+            prioritySum += priority[i.first!]!
+        }
+        return "\(prioritySum)"
+    }
+
+
+    func part2(parts: [String]) -> String {
+        var prioritySum = 0
+        assert(parts.count % 3 == 0)
+        var all: Set<Character> = []
+        for (i, part) in parts.enumerated() {
+            switch i % 3 {
+            case 0:
+                all = Set(part)
+            case 1:
+                all = all.intersection(Set(part))
+            case 2:
+                all = all.intersection(Set(part))
+                assert(all.count == 1)
+                prioritySum += priority[all.first!]!
+            default:
+                break
+            }
+        }
+        return "\(prioritySum)"
     }
 }
