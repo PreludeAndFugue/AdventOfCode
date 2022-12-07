71c3cbd8c3f31c81e430605fcdff4941e75c87f4
Gary Kerr <gary.kerr@tutanota.com>
2022-12-07 23:16:54 +0000

    2022, day 7, part 2, python

--- 2022/python/day07.py
+++ 2022/python/day07.py
@@ -14,8 +14,14 @@ directory = {
     "files": [(size, name), ...]
 }
 
-
+Part 1
+------
 1190023: too low
+
+
+Part 2
+------
+11_285_770: too high
 
 '''
 
@@ -69,7 +75,7 @@ def directory_sizes(directory, directory_name, path, current_sizes):
     return s
 
 
-def part1(s):
+def make_file_system(s):
     root = {
         'files': []
     }
@@ -93,24 +99,37 @@ def part1(s):
             size, name = line.split(' ')
             size = int(size)
             insert_file((size, name), directory_stack, root)
+    return root
 
-    all_sizes = {}
-    directory_sizes(root, '/', '', all_sizes)
 
-    total = 0
-    for v in all_sizes.values():
-        if v <= 100_000:
-            total += v
-    return total
+def part1(all_sizes):
+    return sum(v for v in all_sizes.values() if v <= 100_000)
+
+
+def part2(all_sizes):
+    total_space = 70_000_000
+    unused_space_required = 30_000_000
+    used_space = all_sizes['/']
+    unused_space = total_space - used_space
+    space_required = unused_space_required - unused_space
+    values = [n for n in all_sizes.values() if n >= space_required]
+    values.sort()
+    return values[0]
 
 
 def main():
     s = get_input('07').split('\n')
     # s = TEST.strip().split('\n')
 
-    p1 = part1(s)
+    file_system = make_file_system(s)
+    all_sizes = {}
+    directory_sizes(file_system, '/', '', all_sizes)
+
+    p1 = part1(all_sizes)
+    p2 = part2(all_sizes)
 
     print('Part 1:', p1)
+    print('Part 2', p2)
 
 
 if __name__ == '__main__':
