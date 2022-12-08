3bd1355200af1dd26341e84507f3cc9d35079a04
Gary Kerr <gary.kerr@tutanota.com>
2022-12-08 23:44:10 +0000

    2022, day 8, part 2, python

--- 2022/python/day08.py
+++ 2022/python/day08.py
@@ -1,6 +1,12 @@
 
 from help import get_input
 
+'''
+Part 2
+------
+700 too low
+'''
+
 
 TEST = '''30373
 25512
@@ -16,29 +22,58 @@ def part1(grid):
         if h > height:
             can_see.add((row, col))
             height = h
-            
+
     columns = len(grid[0])
     rows = len(grid)
     can_see = set()
-    for row in range(columns):
+    for row in range(rows):
         height = -1
-        for col in range(rows):
+        for col in range(columns):
             check(row, col)
 
         height = -1
-        for col in reversed(range(rows)):
+        for col in reversed(range(columns)):
             check(row, col)
 
-    for col in range(rows):
+    for col in range(columns):
         height = -1
-        for row in range(columns):
+        for row in range(rows):
             check(row, col)
 
         height = -1
-        for row in reversed(range(columns)):
+        for row in reversed(range(rows)):
             check(row, col)
 
     return len(can_see)
+
+
+def scenic_score(row, col, grid):
+    height = grid[row][col]
+    result = 1
+    # up, left, right, down
+    for dr, dc in ((-1, 0), (0, -1), (0, 1), (1, 0)):
+        count = 0
+        r, c = row, col
+        while True:
+            r += dr
+            c += dc
+            if r < 0 or c < 0:
+                break
+            try:
+                h = grid[r][c]
+            except IndexError:
+                break
+            count += 1
+            if h >= height:
+                break
+        result *= count
+    return result
+
+
+def part2(grid):
+    c = len(grid[0])
+    r = len(grid)
+    return max(scenic_score(row, col, grid) for row in range(r) for col in range(c))
 
 
 def main():
@@ -47,8 +82,10 @@ def main():
     grid = list(list(map(int, l))for l in s.strip().split('\n'))
 
     p1 = part1(grid)
+    p2 = part2(grid)
 
     print('Part 1:', p1)
+    print('Part 2:', p2)
 
 
 if __name__ == '__main__':
