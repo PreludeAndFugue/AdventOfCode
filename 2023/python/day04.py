255694428faf2325b20265ebaffaa0c11eff8625
Gary Kerr <gary.kerr@tutanota.com>
2023-12-04 14:26:24 +0000

    2023, day 4, part 2

--- 2023/python/day04.py
+++ 2023/python/day04.py
@@ -1,3 +1,5 @@
+
+from collections import Counter
 from help import get_input
 
 TEST = '''Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
@@ -8,20 +10,49 @@ Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11'''
 
 
-def main():
-    d = get_input('04').strip().split('\n')
-    # d = TEST.strip().split('\n')
-    t = 0
+def parse(d):
     for l in d:
         a, b = l.split(' | ')
         a1, a2 = a.split(':')
         card_no = int(a1.strip().split()[1])
         winning_numbers = set(int(x) for x in a2.strip().split())
         numbers = set(int(x) for x in b.strip().split())
+        yield card_no, winning_numbers, numbers
+
+
+def part1(cards):
+    t = 0
+    for _, winning_numbers, numbers in cards:
         n = len(numbers.intersection(winning_numbers))
         p = 0 if n == 0 else 2**(n - 1)
         t += p
-    print(t)
+    return t
+
+
+def part2(cards):
+    counter = Counter()
+    for card_no, winning_numbers, numbers in cards:
+        counter[card_no] += 1
+        card_no_count = counter[card_no]
+        n = len(numbers.intersection(winning_numbers))
+        if n > 0:
+            for i in range(card_no + 1, card_no + n + 1):
+                counter[i] += card_no_count
+    return sum(counter.values())
+
+
+def main():
+    d = get_input('04').strip().split('\n')
+    # d = TEST.strip().split('\n')
+    cards = list(parse(d))
+
+    p1 = part1(cards)
+    print(p1)
+
+    p2 = part2(cards)
+    print(p2)
+
+
 
 
 if __name__ == '__main__':
