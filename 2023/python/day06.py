4601a86b6ea9cf087bd688f5c4dd9a0a111cbe3f
Gary Kerr <gary.kerr@tutanota.com>
2023-12-06 20:18:04 +0000

    2023, day 6, refactor

--- 2023/python/day06.py
+++ 2023/python/day06.py
@@ -6,29 +6,26 @@ TEST = '''Time:      7  15   30
 Distance:  9  40  200'''
 
 
+
 def main():
     d = get_input('06').strip()
     # d = TEST.strip()
     times, distances = d.split('\n')
-    times = [int(n) for n in times.split(':')[1].strip().split()]
-    distances = [int(n) for n in distances.split(':')[1].strip().split()]
 
-    max_ss = []
-    for t, d in zip(times, distances):
-        c = Counter()
-        for v in range(t + 1):
-            dt = t - v
-            s = v * dt
-            c[s] += 1
-            print('\t', v,  dt, s)
-        w = 0
-        for k, v in c.items():
-            if k > d:
-                w += v
-        max_ss.append(w)
+    t1 = [int(n) for n in times.split(':')[1].strip().split()]
+    d1 = [int(n) for n in distances.split(':')[1].strip().split()]
+
+    t2 = int(''.join(times.split(':')[1].strip().split()))
+    d2 = int(''.join(distances.split(':')[1].strip().split()))
+
+    ws = []
+    for t, d in zip(t1, d1):
+        c = Counter(v*(t - v) for v in range(t + 1))
+        w = sum(v for k, v in c.items() if k > d)
+        ws.append(w)
 
     n = 1
-    for m in max_ss:
+    for m in ws:
         n *= m
     print(n)
 
