530fb01ec8277010bc1d7488dd423d59270167cd
Gary Kerr <gary.kerr@tutanota.com>
2023-12-08 20:21:59 +0000

    2023, day 8, part 2

--- 2023/python/day08.py
+++ 2023/python/day08.py
@@ -1,7 +1,7 @@
 
 from itertools import cycle
 
-from help import get_input
+from help import get_input, lcm
 
 TEST1 = '''RL
 
@@ -20,28 +20,54 @@ BBB = (AAA, ZZZ)
 ZZZ = (ZZZ, ZZZ)
 '''
 
+NS = {'L': 0, 'R': 1}
 
-d = get_input('08').strip()
-# d = TEST1.strip()
-# d = TEST2.strip()
-directions, b = d.split('\n', maxsplit=1)
-directions = directions.strip()
+def parse(d):
+    directions, b = d.split('\n', maxsplit=1)
+    directions = directions.strip()
 
-m = {}
-for l in b.strip().split('\n'):
-    p = l.split(' = ')
-    x, y = p[0], p[1]
-    y = y.replace('(', '')
-    y = y.replace(')', '')
-    y = y.split(', ')
-    m[x] = y
+    m = {}
+    for l in b.strip().split('\n'):
+        p = l.split(' = ')
+        x, y = p[0], p[1]
+        y = y.replace('(', '')
+        y = y.replace(')', '')
+        y = y.split(', ')
+        m[x] = y
+    return directions, m
 
-ns = {'L': 0, 'R': 1}
-p = 'AAA'
-c = 0
-for d in cycle(directions):
-    p = m[p][ns[d]]
-    c += 1
-    print(p, d, c)
-    if p == 'ZZZ':
-        break
+
+def part1(directions, m, start, endf):
+    p = start
+    for i, d in enumerate(cycle(directions), start=1):
+        n = NS[d]
+        p = m[p][n]
+        if endf(p):
+            return i
+
+
+def part2(directions, m):
+    starts = [m for m in m.keys() if m.endswith('A')]
+    l = 1
+    for p in starts:
+        c = part1(directions, m, p, lambda p: p.endswith('Z'))
+        l = lcm(l, c)
+    return l
+
+
+def main():
+    d = get_input('08').strip()
+    # d = TEST1.strip()
+    # d = TEST2.strip()
+
+    directions, m = parse(d)
+
+    p1 = part1(directions, m, 'AAA', lambda p: p == 'ZZZ')
+    print(p1)
+
+    p2 = part2(directions, m)
+    print(p2)
+
+
+if __name__ == '__main__':
+    main()
--- 2023/python/help.py
+++ 2023/python/help.py
@@ -3,3 +3,15 @@ base_path = '../day{}.txt'
 def get_input(d):
     path = base_path.format(d)
     return open(path, 'r').read().strip()
+
+
+def gcd(a, b):
+    a = abs(a)
+    b = abs(b)
+    while b > 0:
+        a, b = b, a % b
+    return a
+
+
+def lcm(a, b):
+    return abs(a * b) // gcd(a, b)
