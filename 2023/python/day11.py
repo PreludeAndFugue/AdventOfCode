cf1e607c5f003385af6e3da14d5cb48c26b8cb3c
Gary Kerr <gary.kerr@tutanota.com>
2023-12-11 16:08:49 +0000

    2023, day 11, part 2

--- 2023/python/day11.py
+++ 2023/python/day11.py
@@ -44,25 +44,67 @@ def expand(x):
     return x
 
 
+def get_galaxies(x):
+    galaxies = []
+    for r, row in enumerate(x):
+        for c, ch in enumerate(row):
+            if ch == '#':
+                galaxies.append((r, c))
+    return galaxies
+
+
+def distance(galaxies):
+    total_d = 0
+    for g1, g2 in combinations(galaxies, 2):
+        r1, c1 = g1
+        r2, c2 = g2
+        d = abs(r1 - r2) + abs(c1 - c2)
+        total_d += d
+    return total_d
+
+
+def expand_2(galaxies, empty_r, empty_c, n):
+    expanded_galaxies = []
+    for galaxy in galaxies:
+        r, c = galaxy
+        extra_r = (n - 1)*sum(1 for m in empty_r if m < r)
+        extra_c = (n - 1)*sum(1 for m in empty_c if m < c)
+        expanded_galaxies.append((r + extra_r, c + extra_c))
+    return expanded_galaxies
+
+
+def part1(x):
+    x = expand(x)
+
+    # test = '\n'.join(''.join(a) for a in x)
+    # assert test == TEST_EXPAND
+
+    galaxies = get_galaxies(x)
+    return distance(galaxies)
+
+
+def part2(x):
+    empty_r = set()
+    for r, row in enumerate(x):
+        if len(set(row)) == 1:
+            empty_r.add(r)
+
+    empty_c = set()
+    for c, row in enumerate(zip(*x)):
+        if len(set(row)) == 1:
+            empty_c.add(c)
+
+    galaxies = get_galaxies(x)
+    expanded_galaxies = expand_2(galaxies, empty_r, empty_c, 1_000_000)
+    return distance(expanded_galaxies)
+
+
 d = get_input('11').strip()
 # d = TEST.strip()
 x = [tuple(a) for a in d.split('\n')]
-x = expand(x)
 
-# test = '\n'.join(''.join(a) for a in x)
-# assert test == TEST_EXPAND
+p1 = part1(x)
+print(p1)
 
-galaxies = []
-for r, row in enumerate(x):
-    for c, ch in enumerate(row):
-        if ch == '#':
-            galaxies.append((r, c))
-
-total_d = 0
-for g1, g2 in combinations(galaxies, 2):
-    r1, c1 = g1
-    r2, c2 = g2
-    d = abs(r1 - r2) + abs(c1 - c2)
-    total_d += d
-
-print(total_d)
+p2 = part2(x)
+print(p2)
