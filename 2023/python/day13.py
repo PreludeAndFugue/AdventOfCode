09dbd708590ab3127d9eca93992fd81eff857fce
Gary Kerr <gary.kerr@tutanota.com>
2023-12-14 22:24:22 +0000

    2023, day 13, part 2, work in progress

--- 2023/python/day13.py
+++ 2023/python/day13.py
@@ -18,17 +18,30 @@ TEST = '''#.##..##.
 #....#..#'''
 
 
-d = get_input('13').strip()
-# d = TEST.strip()
+# d = get_input('13').strip()
+d = TEST.strip()
 
-def find_reflection_line(lines):
+def find_diff_1(l1, l2):
+    d = 0
+    for c1, c2 in zip(l1, l2):
+        if c1 != c2:
+            d += 1
+    print('difs')
+    print(l1)
+    print(l2)
+    print(d)
+    print()
+    return d == 1
+
+
+def find_reflection_line(lines, fn):
     ii = []
     for i, (l1, l2) in enumerate(zip(lines, lines[1:])):
-        if l1 == l2:
+        if fn(l1, l2):
             ii.append(i)
     for i in ii:
-        ls1 = reversed(lines[:i + 1])
-        ls2 = lines[i + 1:]
+        ls1 = reversed(lines[:i])
+        ls2 = lines[i + 2:]
         a = all(l1 == l2 for l1, l2 in zip(ls1, ls2))
         if a:
             return 'H', i + 1
@@ -36,11 +49,11 @@ def find_reflection_line(lines):
     columns = list(zip(*lines))
     jj = []
     for j, (c1, c2) in enumerate(zip(columns, columns[1:])):
-        if c1 == c2:
+        if fn(c1, c2):
             jj.append(j)
     for j in jj:
-        cs1 = reversed(columns[:j + 1])
-        cs2 = columns[j + 1:]
+        cs1 = reversed(columns[:j])
+        cs2 = columns[j + 2:]
         a = all(c1 == c2 for c1, c2 in zip(cs1, cs2))
         if a:
             return 'V', j + 1
@@ -51,7 +64,8 @@ def find_reflection_line(lines):
 p1 = 0
 for pattern in d.split('\n\n'):
     lines = pattern.split('\n')
-    d, n = find_reflection_line(lines)
+    fn = lambda l1, l2: l1 == l2
+    d, n = find_reflection_line(lines, find_diff_1)
     if d == 'H':
         p1 += 100 * n
     else:
