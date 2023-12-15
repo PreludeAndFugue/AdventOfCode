12d3fbadb6bf5c30a013d536c0bd099d1738f042
Gary Kerr <gary.kerr@tutanota.com>
2023-12-15 21:31:01 +0000

    2023, day 15, part 2

--- 2023/python/day15.py
+++ 2023/python/day15.py
@@ -1,4 +1,6 @@
 
+from collections import defaultdict, OrderedDict
+
 from help import get_input
 
 TEST = '''rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7'''
@@ -17,7 +19,32 @@ def hash(value):
 
 
 p1 = 0
+boxes = defaultdict(OrderedDict)
 for step in d.split(','):
     h = hash(step)
     p1 += h
+
+    if '=' in step:
+        l, f = step.split('=')
+        f = int(f)
+        h = hash(l)
+        boxes[h][l] = f
+    else:
+        l = step.split('-')[0]
+        h = hash(l)
+        box = boxes[h]
+        if l in box:
+            del box[l]
+
+p2 = 0
+for i in range(256):
+    box = boxes[i]
+    slot = 0
+    for l, f in box.items():
+        slot += 1
+        p2 += (i + 1)*slot*f
+
+
 print(p1)
+
+print(p2)
