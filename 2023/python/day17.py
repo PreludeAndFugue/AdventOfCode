5cc97f3a7cf0f0a05c1d8f8b8252164952610470
Gary Kerr <gary.kerr@tutanota.com>
2023-12-17 19:23:10 +0000

    2023, day 17, part 2, work in progress

--- 2023/python/day17.py
+++ 2023/python/day17.py
@@ -22,15 +22,11 @@ TEST = '''2413432311323
 2546548887735
 4322674655533'''
 
-
-# d = get_input('17')
-d = TEST.strip()
-
-map_ = {}
-for r, line in enumerate(d.split('\n')):
-    for c, n in enumerate(line):
-        # print(r, c, n)
-        map_[(r, c)] = int(n)
+TEST_2 = '''111111111111
+999999999991
+999999999991
+999999999991
+999999999991'''
 
 LEFT = {
     (0, 1): (-1, 0),
@@ -44,6 +40,16 @@ RIGHT = {
     (0, -1): (-1, 0),
     (-1, 0): (0, 1)
 }
+
+
+def make_map(d):
+    map_ = {}
+    for r, line in enumerate(d.split('\n')):
+        for c, n in enumerate(line):
+            # print(r, c, n)
+            map_[(r, c)] = int(n)
+    return map_
+
 
 def get_next(state, map_):
     h, p, d, l = state
@@ -72,6 +78,51 @@ def get_next(state, map_):
     if hh is not None:
         yield h + hh, pp, dd, 0
 
+
+def get_next_2(state, map_):
+    h, p, d, l = state
+    if l < 3:
+        # continue straight on
+        r, c = p
+        dr, dc = d
+        pp = r + dr, c + dc
+        hh = map_.get(pp, None)
+        if hh is not None:
+            yield h + hh, pp, d, l + 1
+    else:
+        if l < 10:
+            # continue straight on
+            r, c = p
+            dr, dc = d
+            pp = r + dr, c + dc
+            hh = map_.get(pp, None)
+            if hh is not None:
+                yield h + hh, pp, d, l + 1
+        # turn left
+        dd = LEFT[d]
+        r, c = p
+        dr, dc = dd
+        pp = r + dr, c + dc
+        hh = map_.get(pp, None)
+        if hh is not None:
+            yield h + hh, pp, dd, 0
+        # turn right
+        dd = RIGHT[d]
+        r, c = p
+        dr, dc = dd
+        pp = r + dr, c + dc
+        hh = map_.get(pp, None)
+        if hh is not None:
+            yield h + hh, pp, dd, 0
+
+
+# d = get_input('17')
+# d = TEST.strip()
+d = TEST_2.strip()
+
+map_ = make_map(d)
+
+
 start_p = 0, 0
 end_p = max(k for k in map_.keys())
 # print(start_p, end_p)
@@ -87,7 +138,6 @@ while check:
     state = heapq.heappop(check)
 
     h, (r, c), d, l = state
-    state = h, (-r, -c), d, l
 
     i += 1
     if i % 100_000 == 0:
@@ -95,9 +145,9 @@ while check:
         print(len(check))
 
     # print('state', state)
-    _, p, _, _ = state
+    _, p, _, l = state
 
-    if p == end_p:
+    if p == end_p and l > 2:
         print('done', h)
         break
 
@@ -108,9 +158,10 @@ while check:
 
     seen.add(state)
 
-    for new_state in get_next(state, map_):
+    # for new_state in get_next(state, map_):
+    for new_state in get_next_2(state, map_):
         # print('\tnext', new_state)
         h, (r, c), d, l = new_state
-        ns = h, (-r, -c), d, l
+        ns = h, (r, c), d, l
         heapq.heappush(check, ns)
 
