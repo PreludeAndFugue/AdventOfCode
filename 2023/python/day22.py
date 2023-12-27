9aedbd137efc254a07d37c7e1ab087ccd651eea4
Gary Kerr <gary.kerr@tutanota.com>
2023-12-27 16:43:30 +0000

    2023, day 22, refactor part 1

--- 2023/python/day22.py
+++ 2023/python/day22.py
@@ -1,4 +1,5 @@
 
+from collections import defaultdict
 from itertools import combinations
 
 from help import get_input
@@ -25,27 +26,14 @@ class Brick:
         self.y = y
         self.z = z
 
-    def can_fall(self):
-        return self.z.start > 1
-
-    def fall(self):
-        self.z = range(self.z.start - 1, self.z.stop - 1)
-
-    def test_fall(self):
-        z = range(self.z.start - 1, self.z.stop - 1)
-        return Brick(self.n, self.x, self.y, z)
-
-    def intersects(self, other):
+    def interects_x_y(self, other):
         x = bool(intersection(self.x, other.x))
         if not x: return False
         y = bool(intersection(self.y, other.y))
-        if not y: return False
-        z = bool(intersection(self.z, other.z))
-        return z
-        # return x and y and z
+        return y
 
     def __repr__(self) -> str:
-        return f'{self.n}: {self.x.start},{self.y.start},{self.z.start}~{self.x.stop - 1},{self.y.stop - 1},{self.z.stop - 1}'
+        return f'{self.n}: {self.x.start}-{self.x.stop - 1},{self.y.start}-{self.y.stop - 1},{self.z.start}-{self.z.stop - 1}'
 
 
 def parse(d):
@@ -56,73 +44,66 @@ def parse(d):
         yield Brick(i, range(a[0], b[0] + 1), range(a[1], b[1] + 1), range(a[2], b[2] + 1))
 
 
-def get_fallers(bricks):
-    for b in bricks:
-        if b.can_fall():
-            yield b
-
-
-def find_faller(bricks):
-    '''A single fall step for all bricks'''
-    fallers = list(get_fallers(bricks))
-    # print('fallers', len(fallers))
-    if not fallers:
-        return None
-    for f in fallers:
-        # print("fall test", f.n)
-        f_test = f.test_fall()
-        intersections = []
-        for other in bricks:
-            if other.n == f_test.n:
-                continue
-            intersections.append(f_test.intersects(other))
-        # print('fall intersections', intersections)
-        if not any(intersections):
-            return f
-    return None
-
-
-def part1(d):
-    def process_fall(b, bricks):
-        # print('process fall', b)
-        did_fall = False
-        while True:
-            # print('\tcan fall', b.can_fall())
-            if not b.can_fall():
-                return did_fall
-            test_b = b.test_fall()
-            for other in bricks:
-                if other.n == test_b.n: continue
-                if test_b.intersects(other):
-                    return did_fall
-            b.fall()
-            # print('\tfalling', b)
-            did_fall = True
-
-    bricks = list(parse(d))
-    bricks = sorted(bricks, key=lambda b: b.z.start)
-
-    repeat = True
-    while repeat:
-        repeat = any(process_fall(b, bricks) for b in bricks)
+def do_fall_a(d):
+    bricks = sorted(list(parse(d)), key=lambda b: b.z.start)
+    for i, b in enumerate(bricks):
+        if b.z.start == 1: continue
+        zs = []
+        for other in bricks[:i]:
+            if b.interects_x_y(other):
+                zs.append(other.z)
+        if zs:
+            z_start = max(z.stop for z in zs)
+            # drop to rest on block
+            l = b.z.stop - b.z.start
+            b.z = range(z_start, z_start + l)
+        else:
+            # drop to rest on ground
+            r = b.z
+            b.z = range(1, r.stop - r.start + 1)
 
     return bricks
+
+
+def make_dependencies(bricks):
+    supports = defaultdict(list)
+    rests_on = defaultdict(list)
+    for b in bricks:
+        for other in bricks:
+            if other.n == b.n: continue
+            if not b.interects_x_y(other): continue
+            if b.z.stop == other.z.start:
+                supports[b.n].append(other)
+                rests_on[other.n].append(b)
+    return supports, rests_on
 
 
 def main():
     d = get_input('22').strip()
     # d = TEST.strip()
 
-    bricks = part1(d)
+    bricks = do_fall_a(d)
 
-    can_disintigrate = []
+    bricks = sorted(bricks, key=lambda b: (b.z.start, b.x.start, b.y.start))
+    brick_dict = {b.n: b for b in bricks}
+
+    supports, rests_on = make_dependencies(bricks)
+
+    can_disintigrate = set()
     for b in bricks:
-        others = bricks[:b.n] + bricks[b.n + 1:]
-        f = find_faller(others)
-        if f is None:
-            can_disintigrate.append(b)
-
-    print('can disintigrate')
+        supported = supports[b.n]
+        if not supported:
+            can_disintigrate.add(b.n)
+            continue
+        tests = []
+        for s in supported:
+            supporters = rests_on[s.n]
+            if len(supporters) > 1:
+                tests.append(True)
+            else:
+                tests.append(False)
+        if all(tests):
+            can_disintigrate.add(b.n)
     print(len(can_disintigrate))
 
 
