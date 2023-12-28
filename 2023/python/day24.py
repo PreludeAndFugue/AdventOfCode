baf287ad83cf0296c3708da07c22703207f173f3
Gary Kerr <gary.kerr@tutanota.com>
2023-12-28 01:46:55 +0000

    2023, day 24, part 1, refactor

--- 2023/python/day24.py
+++ 2023/python/day24.py
@@ -14,6 +14,33 @@ m = dy/dx
 c = y - dy/dx*x
 
 Input data has dx != 0 for all cases.
+
+For part 1
+----------
+
+Use parametric equations for line:
+x0 + dx0*t0 = x1 + dx1*t1
+y0 + dy0*t0 = y1 + dy1*t1
+
+x0, y0, dx0, dy0, x1, y1, dx1, dy1 are known.
+Solve for t0 and t1.
+
+For part 2
+----------
+
+P = (x, y, z) is position of stone at t = 0
+V = (dx, dy, dz) is velocity of stone
+
+Solve parametric equations. Three vector equations - or 9 equations in total.
+
+P + V*T1 = p1 + v1*T1
+P + V*T2 = p2 + v2*T2
+P + V*T3 = p3 + v3*T3
+
+p1 and v1 are position (at t = 0) and velocity of hailstone 1, etc.
+
+Unknows P (3 variables), V (3 variables), T1, T2, and T3.
+
 '''
 
 TEST = '''
@@ -39,29 +66,8 @@ class Hailstone:
         self.m = Fraction(dy, dx)
         self.c = y - self.m*x
 
-    def intersection(self, other):
-        if self.m == other.m:
-            if self.c == other.c:
-                # This point is not in the past for this hailstone, but it could be for
-                # the other hailstone.
-                return self.x, self.y
-            else:
-                return None
-        dm = self.m - other.m
-        x = (other.c - self.c)/dm
-        y = (other.c*self.m - self.c*other.m)/dm
-        return x, y
-
-    def point_in_past(self, x, y):
-        if x < self.x and self.dx > 0:
-            return True
-        if x > self.x and self.dx < 0:
-            return True
-        if y < self.y and self.dy > 0:
-            return True
-        if y > self.y and self.dy < 0:
-            return True
-        return False
+    def p(self, t):
+        return self.x + t*self.dx, self.y + t*self.dy
 
 
 def parse(d):
@@ -72,30 +78,46 @@ def parse(d):
         yield Hailstone(x, y, dx, dy)
 
 
-def main():
-    d = get_input('24')
-    # d = TEST.strip()
+def solve_for_t(h1, h2):
+    n = (h2.x - h1.x)*h2.dy - (h2.y - h1.y)*h2.dx
+    d = h1.dx*h2.dy - h2.dx*h1.dy
+    if d == 0:
+        return None
+    t1 = Fraction(n, d)
+    t2 = Fraction(h1.x - h2.x, h2.dx) + t1*Fraction(h1.dx, h2.dx)
+    return t1, t2
+
+
+def part1(d):
     hs = list(parse(d))
 
     MA = MAX
     MI = MIN
+    # MA = MAX_TEST
+    # MI = MIN_TEST
 
     total = 0
-    for h1, h2 in combinations(hs, 2):
-        i = h1.intersection(h2)
-        if i is None:
+    for h0, h1 in combinations(hs, 2):
+        ts = solve_for_t(h0, h1)
+        if ts is None:
             continue
-        x, y = i
-        if h1.point_in_past(x, y):
+        t0, t1 = ts
+        if t0 < 0 or t1 < 0:
             continue
-        if h2.point_in_past(x, y):
-            continue
-        if x < MI or x > MA:
-            continue
-        if y < MI or y > MA:
-            continue
-        total += 1
-    print(total)
+        x0, y0 = h0.p(t0)
+        x1, y1 = h1.p(t1)
+        if MI <= x0 <= MA and MI <= y0 <= MA and MI <= x1 <= MA and MI <= y1 <= MA:
+            total += 1
+    return total
+
+
+def main():
+    d = get_input('24')
+    # d = TEST.strip()
+
+    p1 = part1(d)
+    print(p1)
+
 
 
 if __name__ == '__main__':
