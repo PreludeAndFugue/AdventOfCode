8df4326b8f67c5a7f2ecc90b142667c742618f99
Gary Kerr <gary.kerr@tutanota.com>
2023-12-31 20:55:39 +0000

    2018, day 16, part 2

--- 2018/python/day16.py
+++ 2018/python/day16.py
@@ -1,8 +1,10 @@
 
+from collections import defaultdict
+
 from help import get_input
 
 def parse(d):
-    a, b = d.split('\n\n\n')
+    a, b = d.split('\n\n\n\n')
     a = a.split('\n\n')
     c = []
     for x in a:
@@ -77,27 +79,58 @@ opcodes = [
 
 def part1(tests):
     answer = 0
+    opcode_ids = defaultdict(set)
     for before, op, after in tests:
-        count = 0
-        for opcode in opcodes:
+        count =0
+        for i, opcode in enumerate(opcodes):
             registers = {i: before[i] for i in range(4)}
-            _, a, b, c = op
+            opcode_id, a, b, c = op
             opcode(a, b, c, registers)
             result = [registers[i] for i in range(4)]
             if result == after:
                 count += 1
+                opcode_ids[opcode_id].add(i)
         if count >= 3:
             answer += 1
+    return answer, opcode_ids
 
-    return answer
+
+def part2(opcode_dict, instructions):
+    result = {}
+    kk = None
+    vv = None
+    for _ in range(16):
+        for k, v in opcode_dict.items():
+            if len(v) == 1:
+                kk = k
+                vv = v.pop()
+                result[k] = vv
+                break
+        del opcode_dict[kk]
+        for v in opcode_dict.values():
+            if vv in v:
+                v.remove(vv)
+    new_opcodes = {}
+    for k, v in result.items():
+        new_opcodes[k] = opcodes[v]
+
+    registers = {i: 0 for i in range(4)}
+    for instruction in instructions:
+        i, a, b, c = instruction
+        opcode = new_opcodes[i]
+        opcode(a, b, c, registers)
+    return registers[0]
 
 
 def main():
     d = get_input('16').strip()
     tests, instructions = parse(d)
 
-    p1 = part1(tests)
+    p1, opcode_dict = part1(tests)
     print(p1)
+
+    p2 = part2(opcode_dict, instructions)
+    print(p2)
 
 
 if __name__ == '__main__':
