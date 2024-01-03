a49beec1e6a4353c85f911d220e29a1066f43f64
Gary Kerr <gary.kerr@tutanota.com>
2024-01-03 23:54:21 +0000

    2023, refactor

--- 2023/swift/AdventOfCode2023_CmdLine/AdventOfCode2023_CmdLine/main.swift
+++ 2023/swift/AdventOfCode2023_CmdLine/AdventOfCode2023_CmdLine/main.swift
@@ -5,18 +5,4 @@
 //  Created by gary on 03/01/2024.
 //
 
-import Foundation
-
-
-func getInput(_ day: String) -> String {
-    let fm = FileManager.default
-    let filePath = fm.homeDirectoryForCurrentUser
-        .appending(path: "Documents")
-        .appending(path: "computing")
-        .appending(path: "_AdventOfCode")
-        .appending(path: "2023")
-        .appending(path: "day\(day).txt")
-    return try! String(contentsOf: filePath)
-}
-
 Day01().run()
