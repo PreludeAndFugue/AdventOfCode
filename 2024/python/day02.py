6416858e4e87905c4baea07f56a70f309d6b583c
Gary Kerr <gary.kerr@tutanota.com>
2024-12-03 23:18:31 +0000

    2024, day 2, part 2

--- 2024/python/day02.py
+++ 2024/python/day02.py
@@ -1,6 +1,8 @@
 
 from help import get_input
 
+from itertools import combinations
+
 test1 = '''7 6 4 2 1
 1 2 7 8 9
 9 7 6 2 1
@@ -8,33 +10,43 @@ test1 = '''7 6 4 2 1
 8 6 4 4 1
 1 3 6 7 9'''
 
+def is_safe(ns):
+    diffs = [n2 - n1 for n1, n2 in zip(ns, ns[1:])]
+    d0 = diffs[0]
+    if d0 == 0:
+        return False
+    is_positive = d0 > 0
+    if abs(d0) > 3:
+        return False
+    for di in diffs[1:]:
+        if di == 0:
+            return False
+        is_p = di > 0
+        if is_p != is_positive:
+            return False
+        if abs(di) > 3:
+            return False
+    return True
+
+
+def is_safe2(ns):
+    if is_safe(ns):
+        return True
+    for c in combinations(ns, len(ns) - 1):
+        if is_safe(list(c)):
+            return True
+    return False
+
 
 safe = 0
+safe2 = 0
 for line in get_input(2).strip().split('\n'):
-# for line in test1.strip().split('\n'):ls
+# for line in test1.strip().split('\n'):
     ns = [int(n) for n in line.split()]
-    direction = None
-    large = False
-    for n1, n2 in zip(ns, ns[1:]):
-        dn = n1 - n2
-        if dn < 0:
-            d = False
-        elif dn > 0:
-            d = True
-        if direction is not None:
-            if d != direction:
-                direction = None
-                break
-        else:
-            direction = d
-
-        m = abs(dn)
-        if m < 1 or m > 3:
-            large = True
-            break
-
-    if not large and direction is not None:
+    if is_safe(ns):
         safe += 1
+    if is_safe2(ns):
+        safe2 += 1
 
-print(safe)
+print(safe, safe2)
 
