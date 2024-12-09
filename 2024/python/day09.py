20ffe725c4ddf3eda82b8cc1257c1a7893266ef9
Gary Kerr <gary.kerr@tutanota.com>
2024-12-09 14:05:34 +0000

    2024, day 9, part 2

--- 2024/python/day09.py
+++ 2024/python/day09.py
@@ -5,6 +5,7 @@ test1 = '''12345'''
 
 test2 = '''2333133121414131402'''
 
+
 def expand(source):
     result = []
     for i, n in enumerate(source):
@@ -30,11 +31,6 @@ def compact(disk_map):
     last_file_no = l // 2
     last_file_size = disk_map[last_file_position]
 
-    # print(disk_map)
-    # print(file_no, file_position, file_size)
-    # print(space_position, space_size)
-    # print(last_file_position, last_file_no, last_file_size)
-
     compacting = True
 
     while compacting:
@@ -44,8 +40,8 @@ def compact(disk_map):
             break
         if file_no > last_file_no:
             break
+
         # the current file
-        # print('the current file', file_no, file_size)
         for _ in range(file_size):
             yield file_no
 
@@ -55,9 +51,7 @@ def compact(disk_map):
         file_size = disk_map[file_position]
 
         # fill space from the last file
-        # print('fill space')
         while space_size:
-            # print('last file', last_file_no, last_file_size)
             if last_file_size:
                 yield last_file_no
                 last_file_size -= 1
@@ -74,18 +68,71 @@ def compact(disk_map):
             # current space filled, find next space
             space_position += 2
             space_size = disk_map[space_position]
-            # print('moving to next space', space_position, space_size)
-
-        # input()
 
 
-# source = test1.strip()
-# source = test2.strip()
-source = get_input(9)
+def compact2(disk_map):
+    new_disk_map = []
+    for i, n in enumerate(disk_map):
+        if i % 2 == 0:
+            new_disk_map.append((i // 2, n))
+        else:
+            new_disk_map.append((-1, n))
 
-disk_map = list(map(int, source))
-t = 0
-for i, n in enumerate(compact(disk_map)):
-    t += i * n
-print(t)
+    files_to_check = reversed(new_disk_map[2::2])
+    for file_no, file_size in files_to_check:
+        update = False
+        for i, item in enumerate(new_disk_map):
+            if item[0] == file_no:
+                break
+            if item[0] == -1:
+                space = item[1]
+                if space >= file_size:
+                    new_space = space - file_size
+                    update = True
+                    break
 
+        if update:
+            new_disk_map.insert(i, (file_no, file_size))
+            new_disk_map.pop(i + 1)
+            if new_space:
+                new_disk_map.insert(i + 1, (-1, new_space))
+
+    seen = set()
+    for file_no, file_size in new_disk_map:
+        if file_no != -1 and file_no not in seen:
+            seen.add(file_no)
+            for _ in range(file_size):
+                yield file_no
+        else:
+            for _ in range(file_size):
+                yield -1
+
+
+def part1():
+    # source = test1.strip()
+    # source = test2.strip()
+    source = get_input(9)
+
+    disk_map = list(map(int, source))
+    t = 0
+    for i, n in enumerate(compact(disk_map)):
+        t += i * n
+    print(t)
+
+
+def part2():
+    # source = test2.strip()
+    source = get_input(9)
+
+    disk_map = list(map(int, source))
+    compact2(disk_map)
+
+    t = 0
+    for i, n in enumerate(compact2(disk_map)):
+        if n != -1:
+            t += i * n
+    print(t)
+
+
+# part1()
+part2()
