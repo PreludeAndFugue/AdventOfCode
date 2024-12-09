d2e3536b535a79d6bf13d8b056fb5d69bd03c897
Gary Kerr <gary.kerr@tutanota.com>
2024-12-09 00:26:30 +0000

    2024, day 8, part 2

--- 2024/python/day08.py
+++ 2024/python/day08.py
@@ -17,6 +17,17 @@ test1 = '''............
 ............
 ............'''
 
+test2 = '''T.........
+...T......
+.T........
+..........
+..........
+..........
+..........
+..........
+..........
+..........'''
+
 
 def parse(source):
     map_ = defaultdict(list)
@@ -40,6 +51,29 @@ def get_antinodes(p1, p2):
     yield r2 + dr, c2 + dc
 
 
+def get_antinodes_2(p1, p2, r_max, c_max):
+    r1, c1 = p1
+    r2, c2 = p2
+    dr = r2 - r1
+    dc = c2 - c1
+
+    yield p1
+    while True:
+        r1 = r1 - dr
+        c1 = c1 - dc
+        if r1 < 0 or r1 > r_max or c1 < 0 or c1 > c_max:
+            break
+        yield r1, c1
+
+    yield p2
+    while True:
+        r2 = r2 + dr
+        c2 = c2 + dc
+        if r2 < 0 or r2 > r_max or c2 < 0 or c2 > c_max:
+            break
+        yield r2, c2
+
+
 def filter(antinodes, r_max, c_max):
     '''Remove antinodes that lie outside the bounds of the map.'''
     for p in antinodes:
@@ -49,17 +83,16 @@ def filter(antinodes, r_max, c_max):
         yield p
 
 
-source = test1.strip()
+# source = test1.strip()
 source = get_input(8)
 map_, r_max, c_max = parse(source)
 
 antinodes = set()
 for k, v in map_.items():
     for p1, p2 in combinations(v, 2):
-        ans = list(get_antinodes(p1, p2))
+        ans = list(get_antinodes_2(p1, p2, r_max, c_max))
         antinodes.update(ans)
 
-
-antinodes = list(filter(antinodes, r_max, c_max))
+# antinodes = list(filter(antinodes, r_max, c_max))
 
 print(len(antinodes))
