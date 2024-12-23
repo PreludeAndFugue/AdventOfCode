928a83f9a838ea9cfd47133bbcb45879181e80ba
Gary Kerr <gary.kerr@tutanota.com>
2024-12-23 18:21:54 +0000

    2024, day 23

---
+++ 2024/python/day23.py
@@ -0,0 +1,163 @@
+
+from collections import defaultdict
+from itertools import combinations
+
+from help import get_input
+
+test1 = '''kh-tc
+qp-kh
+de-cg
+ka-co
+yn-aq
+qp-ub
+cg-tb
+vc-aq
+tb-ka
+wh-tc
+yn-cg
+kh-ub
+ta-co
+de-co
+tc-td
+tb-wq
+wh-td
+ta-ka
+td-qp
+aq-cg
+wq-ub
+ub-vc
+de-ta
+wq-aq
+wq-vc
+wh-yn
+ka-de
+kh-ta
+co-tc
+wh-qp
+tb-vc
+td-yn'''
+
+test2 = '''
+1-2
+2-3
+3-6
+6-5
+5-4
+4-1'''
+
+test3 = '''
+1-2
+2-3
+3-1
+3-4
+4-5
+5-3'''
+
+
+def parse(source):
+    map_ = defaultdict(list)
+    for line in source.split('\n'):
+        a, b = line.split('-')
+        map_[a].append(b)
+        map_[b].append(a)
+    return map_
+
+
+
+# source = test1.strip()
+# source = test2.strip()
+# source = test3.strip()
+source = get_input(23)
+map_ = parse(source)
+# print(map_)
+
+
+def depth_3_paths(start, map_):
+    d0 = [[start]]
+    d1 = []
+    d2 = []
+    d3 = []
+
+    for path in d0:
+        p = path[-1]
+        for pp in map_[p]:
+            path_ = path.copy()
+            path_.append(pp)
+            d1.append(path_)
+
+    for path in d1:
+        p = path[-1]
+        for pp in map_[p]:
+            if pp == path[0]:
+                continue
+            path_ = path.copy()
+            path_.append(pp)
+            d2.append(path_)
+
+    for path in d2:
+        p = path[-1]
+        for pp in map_[p]:
+            if pp == path[1]:
+                continue
+            path_ = path.copy()
+            path_.append(pp)
+            d3.append(path_)
+
+    for p in d3:
+        if p[3] == p[0]:
+            yield tuple(sorted(p[:3]))
+
+
+cycles = set()
+for k in map_.keys():
+    for p in depth_3_paths(k, map_):
+        cycles.add(p)
+
+t = 0
+for cycle in cycles:
+    # print(cycle)
+    for s in cycle:
+        if s.startswith('t'):
+            # print('\thas t')
+            t += 1
+            break
+print(t)
+
+
+def bron_kerbosch(R, P, X, cliques):
+    """
+    Recursive function to find all maximal cliques.
+    R: current clique
+    P: potential vertices to add to the clique
+    X: vertices already processed
+    """
+    if not P and not X:
+        cliques.append(R)
+        return
+
+    for v in list(P):
+        neighbors = set(map_[v])
+        bron_kerbosch(
+            R.union({v}),
+            P.intersection(neighbors),
+            X.intersection(neighbors),
+            cliques
+        )
+        P.remove(v)
+        X.add(v)
+
+
+def find_cliques(map_):
+    """Find all maximal cliques in the graph."""
+    P = set(map_.keys())
+    R = set()
+    X = set()
+    cliques = []
+    bron_kerbosch(R, P, X, cliques)
+    return cliques
+
+
+c = find_cliques(map_)
+c.sort(key=len)
+cl = c[-1]
+print(','.join(sorted(cl)))
