4bc3600b38a56c6b7a04ad14a630cd5736cc8781
Gary Kerr <gary.kerr@tutanota.com>
2025-12-26 18:50:45 +0000

    2025, day 3, part 2

--- 2025/python/day03.py
+++ 2025/python/day03.py
@@ -21,10 +21,29 @@ def part1():
         n = max(ds[i+1:])
         # print(f' Next max: {n}')
         t += 10*m + n
+    return t
 
+
+def part2():
+    t = 0
+    for line in source.split('\n'):
+        ds = list(map(int, line.strip()))
+        ns = []
+        j = 0
+        for i in range(12):
+            x = len(ds) - (11 - i)
+            m = max(ds[:x])
+            ns.append(m)
+            j = ds.index(m)
+            ds = ds[j+1:]
+        ns = map(str, ns)
+        ns = ''.join(ns)
+        t += int(ns)
     return t
 
 
 if __name__ == '__main__':
     p1 = part1()
     print(p1)
+    p2 = part2()
+    print(p2)
