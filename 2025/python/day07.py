f9a2be0dda4cee2b1a5f56c72fc904caa8e00df6
Gary Kerr <gary.kerr@tutanota.com>
2025-12-27 22:52:13 +0000

    2025, day 7, part 2

--- 2025/python/day07.py
+++ 2025/python/day07.py
@@ -1,4 +1,5 @@
 
+from collections import defaultdict
 from queue import Queue
 
 from help import get_input
@@ -73,6 +74,71 @@ def part1():
     return splits
 
 
+def print_map(map_):
+    rows = []
+    for y in range(16):
+        row = []
+        for x in range(15):
+            row.append(str(map_[(x, y)]))
+        rows.append(' '.join(row))
+    print('\n'.join(rows))
+
+
+def update(p, map_):
+    '''
+    Dynamic programming step to update cells in next row up based on the value in the
+    current cell.
+    '''
+    c = map_[p]
+    if c == '^':
+        return
+    x, y = p
+    left = map_.get((x - 1, y), -1)
+    right = map_.get((x + 1, y ), -1)
+    up = map_.get((x, y - 1), -1)
+    if left == '^':
+        left_up = map_.get((x - 1, y - 1), -1)
+        if left_up != -1 and left_up != '^':
+            map_[(x - 1, y - 1)] += c
+    if right == '^':
+        right_up = map_.get((x + 1, y - 1), -1)
+        if right_up != -1 and right_up != '^':
+            map_[(x + 1, y - 1)] += c
+    if up != '^':
+        map_[(x, y - 1)] = map_[p]
+
+
+
+def part2():
+    '''
+    Dynamic programming. Start from the bottom and work back to the start.
+    '''
+    map_, start = make_map(source)
+    i = max(p[1] for p in map_)
+
+    # Replace '.' with zeros except for the bottom row where replaced by ones.
+    for p in map_:
+        _, y = p
+        if y == i:
+            c = map_[p]
+            if c == '.':
+                map_[p] = 1
+        else:
+            c = map_[p]
+            if c == '.' or c == 'S':
+                map_[p] = 0
+
+    while i >= 0:
+        ps = [p for p in map_ if p[1] == i]
+        for p in ps:
+            update(p, map_)
+        i -= 1
+
+    return map_[start]
+
+
 if __name__ == '__main__':
     p1 = part1()
     print(p1)
+    p2 = part2()
+    print(p2)
