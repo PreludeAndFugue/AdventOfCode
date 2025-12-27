3591f1e8976ba1fbce938b355df3e68b8f8cdef2
Gary Kerr <gary.kerr@tutanota.com>
2025-12-27 15:48:41 +0000

    2025, day 5, part 2

--- 2025/python/day05.py
+++ 2025/python/day05.py
@@ -17,7 +17,7 @@ source = TEST01.strip()
 source = get_input(5)
 
 
-def part1():
+def make(source):
     a, b = source.split('\n\n')
     ranges = []
     for line in a.split('\n'):
@@ -27,10 +27,11 @@ def part1():
     for line in b.split('\n'):
         n = int(line.strip())
         fresh.append(n)
+    return ranges, fresh
 
-    # print(ranges)
-    # print(fresh)
 
+def part1():
+    ranges, fresh = make(source)
     t = 0
     for f in fresh:
         for r in ranges:
@@ -40,7 +41,34 @@ def part1():
     return t
 
 
+def merge_ranges(r1: range, r2: range) -> list[range]:
+    if r2.start <= r1.stop:
+        r = range(r1.start, max(r1.stop, r2.stop))
+        return [r]
+    else:
+        return [r1, r2]
+
+
+def part2():
+    ranges, _ = make(source)
+    ranges.sort(key=lambda r: r.start)
+    i = 0
+    while True:
+        try:
+            r1 = ranges[i]
+            r2 = ranges[i + 1]
+        except IndexError:
+            break
+        result = merge_ranges(r1, r2)
+        if len(result) == 1:
+            ranges[i:i + 2] = result
+        else:
+            i += 1
+    return sum(len(r) for r in ranges)
+
 
 if __name__ == '__main__':
     p1 = part1()
     print(p1)
+    p2 = part2()
+    print(p2)
